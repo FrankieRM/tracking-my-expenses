1a920098500b17238a0329876b5d860daa07afa9
FrankieRM <frodriguezm17@gmail.com>
2019-11-09 14:53:25 -0500

    showing user in menu Account

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -1,27 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiLanguageService } from 'ng-jhipster';
-import { SessionStorageService } from 'ngx-webstorage';
+import { JhiEventManager, JhiLanguageService } from 'ng-jhipster';
+import { Router } from '@angular/router';
 
+import { SessionStorageService } from 'ngx-webstorage';
 import { VERSION } from 'app/app.constants';
 import { JhiLanguageHelper } from 'app/core/language/language.helper';
-import { AccountService } from 'app/core/auth/account.service';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { LoginService } from 'app/core/login/login.service';
 import { ProfileService } from 'app/layouts/profiles/profile.service';
+import { AccountService } from 'app/core/auth/account.service';
+import { Account } from 'app/core/user/account.model';
 
 @Component({
   selector: 'jhi-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['navbar.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  account: Account;
+  authSubscription: Subscription;
+  modalRef: NgbModalRef;
   inProduction: boolean;
   isNavbarCollapsed: boolean;
   languages: any[];
   swaggerEnabled: boolean;
-  modalRef: NgbModalRef;
   version: string;
 
   constructor(
@@ -32,18 +36,32 @@ export class NavbarComponent implements OnInit {
     private accountService: AccountService,
     private loginModalService: LoginModalService,
     private profileService: ProfileService,
-    private router: Router
+    private router: Router,
+    private eventManager: JhiEventManager
   ) {
     this.version = VERSION ? (VERSION.toLowerCase().startsWith('v') ? VERSION : 'v' + VERSION) : '';
     this.isNavbarCollapsed = true;
   }
 
   ngOnInit() {
+    this.accountService.identity().subscribe((account: Account) => {
+      this.account = account;
+    });
+    this.registerAuthenticationSuccess();
+
     this.languages = this.languageHelper.getAll();
 
     this.profileService.getProfileInfo().subscribe(profileInfo => {
       this.inProduction = profileInfo.inProduction;
       this.swaggerEnabled = profileInfo.swaggerEnabled;
+    });
+  }
+
+  registerAuthenticationSuccess() {
+    this.authSubscription = this.eventManager.subscribe('authenticationSuccess', message => {
+      this.accountService.identity().subscribe(account => {
+        this.account = account;
+      });
     });
   }
 
@@ -76,5 +94,11 @@ export class NavbarComponent implements OnInit {
 
   getImageUrl() {
     return this.isAuthenticated() ? this.accountService.getImageUrl() : null;
+  }
+
+  ngOnDestroy() {
+    if (this.authSubscription) {
+      this.eventManager.destroy(this.authSubscription);
+    }
   }
 }
